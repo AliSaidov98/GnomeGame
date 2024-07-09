303984815d59558731fc1ad9aea4df388aaaedf0
Ali <mdsaidov98@gmail.com>
2024-07-09 19:01:18 +0500

    Adding project

---
+++ Source/CoopGnome/CoopGnome.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CoopGnome : ModuleRules
+{
+	public CoopGnome(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/CoopGnome/CoopGnomeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopGnomeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACoopGnomeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACoopGnomeGameMode();
+};
+
+
+
